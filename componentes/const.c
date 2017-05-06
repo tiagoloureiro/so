29855668f3a51c200782f42ea2d51044b4674f45
tiagoloureiro <tiagocostaloureiro@gmail.com>
2017-05-06 23:09:21 +0100

    change own readline to c getline

--- componentes/const.c
+++ componentes/const.c
@@ -4,34 +4,17 @@
 #include <string.h>
 #include <limits.h>
 
-ssize_t readln(int fildes, void *buf, size_t nbyte){
-  ssize_t res = 0;
-  int n;
-  char *b = buf;
-
-  while ((n = read(fildes, b, 1)) > 0){
-    if(res >= nbyte) break;
-    if(n < 0) break;
-    if(*b == '\n') return res;
-    res++;
-    b++;
-  }
-
-  return 0;
-}
-
 int main(int argc, char *argv[]){
-  char buf[PIPE_BUF];
+  char *buf = NULL;
   ssize_t n;
-  ssize_t i = 0;
 
-  while((n = readln(0, buf, sizeof(buf))) > 0){
-    write(1, buf, n);
+  while((n = getline(&buf, &n, stdin)) != -1){
+    write(1, buf, n-1);
 
     if(argc == 2){
-      printf(":%s\n", argv[1]);
+      printf(":%s", argv[1]);
     }
-
+    printf("\n");
   }
 
   return 0;
