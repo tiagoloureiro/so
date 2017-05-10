579f9a1a2a73b053f4cd3d273f92953e631f63ff
tiagoloureiro <tiagocostaloureiro@gmail.com>
2017-05-10 17:57:30 +0100

    tito

--- componentes/const.c
+++ componentes/const.c
@@ -5,17 +5,21 @@
 #include <limits.h>
 
 int main(int argc, char *argv[]){
-  char *buf = NULL;
+  char *buf_r;
+  char buf_w[PIPE_BUF];
+  char aux[PIPE_BUF];
   ssize_t n;
+  int col2;
+  int col4;
+  int total = 0;
 
-  while((n = getline(&buf, &n, stdin)) != -1){
-    write(1, buf, n-1);
-
-    if(argc == 2){
-      printf(":%s", argv[1]);
-    }
-    printf("\n");
+  while((n = getline(&buf_r, &n, stdin)) != -1){
+    snprintf(aux, n, "%s:10", buf_r);
+    total += n + 1 + sizeof(argv[1]);
+    sprintf(buf_w, "%s:%s\n",buf_w,aux, argv[1]);
   }
+
+  printf("%s\n", buf_w);
 
   return 0;
 }
--- componentes/filter.c
+++ componentes/filter.c
@@ -1,49 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <limits.h>
 
-int get_coluna(char *b, ssize_t l, int num){
-  
-  return 4;
+int get_coluna(char *p, int c1, int c2, int* num[2]){
+  char * pch = strtok (p,":");
+  int i;
+  int nu[2] = &num;
+
+  for(i=2; pch, i<=c2; i++){
+    pch = strtok (NULL, ":");
+    if(i==c1){
+      nu[0] = atoi(pch);
+    }
+  }
+
+  nu[1] = atoi(pch);
+
+  return 0;
 }
 
 int main(int argc, char *argv[]){
   char *buf = NULL;
+  char pal[PIPE_BUF];
   ssize_t n;
   int v1, v2;
+  int *num[2];
 
   while((n = getline(&buf, &n, stdin)) != -1){
     if(argc == 4){
-      v1 = get_coluna(&buf, &n, atoi(argv[1]));
-      v2 = get_coluna(&buf, &n, atoi(argv[3]));
+      memcpy(pal, buf, n-1);
+
+      get_coluna(pal, atoi(argv[1]), atoi(argv[3]), num);
+      v1 = num[0];
+      v2 = num[1];
+
+      printf("%d - %d\n", v1, v2);
 
       if(strcmp(argv[2], "<")){
         if(v1 < v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
-      }else if(strcmp(argv[3], "<=")){
+      }else if(strcmp(argv[2], "<=")){
         if(v1 <= v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
-      }else if(strcmp(argv[3], ">=")){
+      }else if(strcmp(argv[2], ">=")){
         if(v1 >= v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
-      }else if(strcmp(argv[3], ">")){
+      }else if(strcmp(argv[2], ">")){
         if(v1 > v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
-      }else if(strcmp(argv[3], "=")){
+      }else if(strcmp(argv[2], "=")){
         if(v1 == v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
-      }else if(strcmp(argv[3], "!=")){
+      }else if(strcmp(argv[2], "!=")){
         if(v1 != v2)
-          write(1, buf, n-1);
+          write(1, buf, n);
 
+      }else{
+        break;
       }
+
+      num[0] = 0;
+      num[1] = 0;
     }
   }
 
