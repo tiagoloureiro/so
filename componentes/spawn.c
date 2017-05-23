5face9bb8e6b12e1c99cb7d609d15d708d105cdf
tiagoloureiro <tiagocostaloureiro@gmail.com>
2017-05-23 23:15:53 +0100

    fixed segmentation fault | finalized

--- componentes/spawn.c
+++ componentes/spawn.c
@@ -8,39 +8,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int join(char * argumentos[], int n, char *str[], char *linha){
-  int coluna;
-
-  for(int i=2; i<n; i++){
-    printf("inicio %d\n", i-2);
-    if(argumentos[i][0] == '$'){
-      printf("\tinicio 1");
-      coluna = atoi(argumentos[i]+1);
-      const char* aux = get_coluna_str(linha, coluna);
-      if(i==2)
-        strcpy(str[i-2], aux);
-      else
-        strcat(str[i-2], aux);
-      printf("\tfim 1");
-    }else{
-      printf("\tinicio 2");
-      if(i==2)
-        strcpy(str[i-2], argumentos[i]);
-      else
-        strcat(str[i-2], argumentos[i]);
-      printf("\tfim 2");
-    }
-
-    if(i != n){
-      printf("\tinicio 3");
-      strcat(str[i-2], " ");
-      printf("\tfim 3");
-    }
-
-    printf("fim %d\n", i-2);
-  }
-}
-
 int main(int argc, char * argv[]){
 
   char *buf = NULL;
@@ -66,11 +33,14 @@ int main(int argc, char * argv[]){
       memcpy(pal, buf, n);
       pal[n-1] = '\0';
 
-      join(argv, argc, comando, pal);
-      //printf("%s %s\n", argv[1], comando);
-
       if(!fork()){
-        execvp(argv[1], comando);
+        for(int i=2; i<argc; i++){
+          if(argv[i][0] == '$'){
+            coluna = atoi(argv[i]+1);
+            strcpy(argv[i], get_coluna_str(pal, coluna));
+          }
+        }
+        execvp(argv[1], argv+1);
         _exit(0);
       }
 
