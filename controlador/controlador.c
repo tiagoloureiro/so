f28851f6b50bde508cfb64b83aa099c8353a25a2
tiagoloureiro <tiagocostaloureiro@gmail.com>
2017-05-24 16:14:01 +0100

    Struct transformed in two arrays

--- controlador/controlador.c
+++ controlador/controlador.c
@@ -8,18 +8,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct comandos{
-  char programa[PIPE_BUF];
-  char argumentos[PIPE_BUF];
-} *COMANDOS;
-
 int main(int argc, char * argv[]){
   char *buf = NULL;
   char **pal = malloc( sizeof( char * ) * PIPE_BUF );
   ssize_t n;
   int tam[PIPE_BUF];
 
-  COMANDOS *cmd;
+  char **programa = malloc( sizeof( char * ) * PIPE_BUF );
+  char **argumentos = malloc( sizeof( char * ) * PIPE_BUF );
 
   int j;
 
@@ -28,41 +24,46 @@ int main(int argc, char * argv[]){
     memcpy(pal[j], buf, n);
     pal[j][n-1] = '\0';
 
-    cmd[j] = malloc(sizeof(COMANDOS));
+    programa[j] = malloc( PIPE_BUF );
+    argumentos[j] = malloc( PIPE_BUF );
 
-    cmd[j]->programa[0] = '.';
-    cmd[j]->programa[1] = '/';
+    programa[j][0] = '.';
+    programa[j][1] = '/';
     for(int k=0; pal[j] && pal[j][k] != ' '; k++)
-      cmd[j]->programa[k+2] = pal[j][k];
+      programa[j][k+2] = pal[j][k];
 
     if(!fork()){
-      if(strcmp(cmd[j]->programa, "node") == 0){
-        strcpy(cmd[j]->argumentos, pal[j]+5);
-        //printf("node %s\n", argumentos);
 
-        execvp(cmd[j]->programa, (char * const*) cmd[j]->argumentos);
-        _exit(0);
-      }else if(strcmp(cmd[j]->programa, "connect") == 0){
-        strcpy(cmd[j]->argumentos, pal[j]+8);
-        //printf("connect %s\n", argumentos);
-      }else if(strcmp(cmd[j]->programa, "disconnect") == 0){
-        strcpy(cmd[j]->argumentos, pal[j]+11);
-        //printf("disconnect %s\n", argumentos);
-      }else if(strcmp(cmd[j]->programa, "inject") == 0){
-        strcpy(cmd[j]->argumentos, pal[j]+7);
-        //printf("inject %s\n", argumentos);
+      if(strcmp(programa[j], "./node") == 0){
+        strcpy(argumentos[j], pal[j]+5);
+        //printf("node %s\n", argumentos[j]);
+        printf("%s\n", argumentos[j]);
+        execvp(programa[j], (char * const*) argumentos[j]);
+      }else if(strcmp(programa[j], "./connect") == 0){
+        strcpy(argumentos[j], pal[j]+8);
+        //printf("connect %s\n", argumentos[j]);
+      }else if(strcmp(programa[j], "./disconnect") == 0){
+        strcpy(argumentos[j], pal[j]+11);
+        //printf("disconnect %s\n", argumentos[j]);
+      }else if(strcmp(programa[j], "./inject") == 0){
+        strcpy(argumentos[j], pal[j]+7);
+        //printf("inject %s\n", argumentos[j]);
       }
+
+      _exit(0);
     }
   }
 
   for(int i=0; i<j; i++){
-    tam[i] = 0;
     int estado;
+
+    char buff[PIPE_BUF];
+    int tam;
+
     wait(&estado);
-    char *buff;
     if (WIFEXITED(estado)){
-      tam[i] += sprintf(buff, "%s -> %s (%d)\n", cmd[j]->programa, cmd[j]->argumentos, estado);
-      write(1, buff, tam[i]);
+      tam = sprintf(buff, "%s -> %s (%d)\n", programa[i], argumentos[i], estado);
+      write(1, buff, tam);
     }
   }
 
