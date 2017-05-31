a5fa6bc9aaf4067acc2e11209aba0a45faf757da
tiagoloureiro <tiagocostaloureiro@gmail.com>
2017-05-31 16:55:17 +0100

    disconnect skeleton

---
+++ controlador/disconnect.c
@@ -0,0 +1,13 @@
+#include "disconnect.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <limits.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int disconnect(char** argumentos){
+  return 1;
+}
---
+++ controlador/disconnect.h
@@ -0,0 +1,1 @@
+int disconnect(char** argumentos);
